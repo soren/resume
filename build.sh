e5684590fa9f896c046b16623c733788bec220e6
Søren Lund <soren@lund.org>
2019-09-05 09:54:20 +0200

    Fix build script to create public directory

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p public/
 for file in cv.tex kompetencer.tex cv.en.tex; do
     date --date "$(git 2>/dev/null log $file|head -n3|awk '/^Date:/{print $2,$3,$4,$5,$6}')" +"%F %H:%M" > ${file}.ts
     pdflatex $file
